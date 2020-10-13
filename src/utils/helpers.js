497eb9847b562d373bd6fe7d900c64449ec9746c
Pataki András <anpataki@gmail.com>
2020-10-14 01:27:27 +0200

    helpers.js - fromSpriteSheet explanation and error message

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -1,8 +1,13 @@
 import * as PIXI from '../pixi';
 
 export const fromSpriteSheet = {
-	defaultSpriteSheet: null,
+	defaultSpriteSheet: null, // gets its value in App.js once the spritesheet finished loading
+
 	create(x, y, width, height, spriteSheet=this.defaultSpriteSheet) {
+		if (!spriteSheet) {
+			console.error('Our spritesheet seems to be missing!');
+			return null;
+		}
 		const spriteTexture = new PIXI.Texture(spriteSheet);
 		spriteTexture.frame = new PIXI.Rectangle(x, y, width, height);
 		const sprite = new PIXI.Sprite(spriteTexture);
