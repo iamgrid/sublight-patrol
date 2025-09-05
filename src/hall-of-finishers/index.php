902c2d6f354dee8e2f8972e7057738070128afbf
Andy Pataki <anpataki@gmail.com>
2025-09-05 20:57:35 +0200

    Hall of Finishers index page and plaque display case is in

--- src/hall-of-finishers/index.php
+++ src/hall-of-finishers/index.php
@@ -1,4 +1,33 @@
 <?php
-	print "hello from index.php!";
-	// phpinfo();
+	require("db_connect.inc");
+	require("renderPlaque.inc");
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="UTF-8" />
+		<title>i.am.grid &middot; Sublight Patrol Hall of Finishers</title>
+		<link
+			href="https://fonts.googleapis.com/css?family=Roboto:300,700&subset=latin-ext"
+			rel="stylesheet"
+		/>
+		<link rel="stylesheet" href="hall-of-finishers.css" />
+	</head>
+	<body class="customScroll">
+		<div class="content">
+		<h1>Sublight Patrol Hall of Finishers</h1>
+<?php
+	print "<div class=\"entries\">\n";
+	
+	$feQuery = "SELECT * FROM `sublight_patrol_finishers` ORDER BY `id` DESC";
+	$feResult = mysqli_query($sp_db, $feQuery);
+	while ($feRow = mysqli_fetch_assoc($feResult)) {
+		renderPlaque($feRow);
+	}
+
+	print "</div>\n";
+?>
+		</div>
+	</body>
+</html>
--- src/hall-of-finishers/plaque.php
+++ src/hall-of-finishers/plaque.php
@@ -1,2 +1,42 @@
 <?php
+	require("db_connect.inc");
+	require("renderPlaque.inc");
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="UTF-8" />
+		<title>i.am.grid &middot; Sublight Patrol Hall of Finishers</title>
+		<link
+			href="https://fonts.googleapis.com/css?family=Roboto:300,700&subset=latin-ext"
+			rel="stylesheet"
+		/>
+		<link rel="stylesheet" href="hall-of-finishers.css" />
+	</head>
+	<body class="customScroll">
+		<div class="content">
+		<h1 class="plaque-display-case-h1">Sublight Patrol Hall of Finishers</h1>
+		<h3 class="plaque-display-case-h3">Plaque Display Case</h3>
+<?php
+	print "<div class=\"entries\">\n";
+	
+	$feQuery = "SELECT * FROM `sublight_patrol_finishers` WHERE `id` = " . intval($_GET['id']);
+	$feResult = mysqli_query($sp_db, $feQuery);
+
+	if (!$feResult || mysqli_num_rows($feResult) < 1) {
+		print "<p class=\"error\">I apologize, I could not locate the requested plaque.</p>\n";
+	} else {
+		$feRow = mysqli_fetch_assoc($feResult);
+		renderPlaque($feRow);
+	}
+
+
+	print "</div>\n";
+?>
+		<div class="return-link-wrapper">
+			<a class="return-link" href="/sublight-patrol/hall-of-finishers/">Return to the main hall</a>
+		</div>
+		</div>
+	</body>
+</html>
